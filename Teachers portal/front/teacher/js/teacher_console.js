7abb68493433179b58352f7c77bf4196b684eb01
Anton Nedogarok <nk260an@gmail.com>
2019-10-27 09:46:30 +0300

    Added teachers backend + minor presentation mod

---
+++ Teachers portal/front/teacher/js/teacher_console.js
@@ -0,0 +1,18 @@
+	// SideNav Button Initialization
+    $(".button-collapse").sideNav();
+    // SideNav Scrollbar Initialization
+    var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+    var ps = new PerfectScrollbar(sideNavScrollbar);
+
+
+    // INITIALIZE ALL LOGIC HERE 
+    $(document).ready(function () {
+
+    });
+    // Data Picker Initialization
+    $('.datepicker').pickadate();
+    // Tooltip Initialization
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+    })
+    
